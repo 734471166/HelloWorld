b8433eafb04c0cffe4076a03cf3505950e0ac88f
MGJrr <734471166@qq.com>
2019-06-14 22:44:07 +0800

    0614

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,8 @@
-public class HelloWorld {
+public class
+HelloWorld {
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println("2");
+        //System.out.println("2");
+
     }
 }
